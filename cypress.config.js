faee7077f53fdf0fdc04bea5faf0432baadf47e2
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2023-09-06 11:23:17 +0800

    Add files

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'regression-suite',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    quiet:true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+    },
+  },
+});
